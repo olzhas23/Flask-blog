7df7042eaced25248a912e0252f7fac4cd2d6961
Olzhas Shaikenov <olzhas.shaikenov@gmail.com>
2014-09-23 17:45:07 -0400

    blog.py  updated files

--- blog.py
+++ blog.py
@@ -3,16 +3,37 @@ from flask import Flask, render_template, request,session, flash, redirect,url_f
 import sqlite3
 
 DATABASE='blog.db'
+USERNAME='admin'
+PASSWORD='admin'
+SECRET_KEY='python'
+
 
 app=Flask(__name__)
 app.config.from_object(__name__)
 
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE.db'])
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+	error=None
+	if request.method == 'POST':
+		if request.form ['username']!=app.config['USERNAME'] or request.form['password']!=app.config['PASSWORD']:
+			error='Invalid Credentials. Please try again'
+		else:
+			session['logged_in']=True
+		return redirect(url_for('main'))
+		
+
+	return render_template('login.html',error=error)
 @app.route('/main')
+@app.route('/logout')
+
+def logout():
+
+	session.pop('logged_in',None)  # pop method is used to reset the session key to default value
+	flash('You were loged out')
+	return redirect(url_for('login'))
+	
 def main():
 	return render_template('main.html')
 	
